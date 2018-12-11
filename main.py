f21080b22575acc1efb3b9938e6ef48a2a2e59bc
paubric <paubric@gmail.com>
2018-12-11 19:56:01 +0200

    Initial

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ file = open('data.txt')
 data = file.read()
 
 data = data.split()
+
+new = random.sample(data, 50)
+print(new[0].capitalize(), *new[1:], '\n')
+
 new = random.sample(data, 100)
-print(*new)
+print(new[0].capitalize(), *new[1:], '\n')
+
+new = random.sample(data, 100)
+print(new[0].capitalize(), *new[1:], '\n')
+
+new = random.sample(data, 50)
+print(new[0].capitalize(), *new[1:], '\n')
